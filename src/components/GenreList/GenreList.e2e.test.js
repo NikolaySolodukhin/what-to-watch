cd2faff38cb0b6705d54b09a0f47cfc7537f77ec
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2019-06-02 16:43:15 +0300

    Добавляет redux store и рефакторит компоненты

---
+++ src/components/GenreList/GenreList.e2e.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {GenresList} from "./GenreList";
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(`<GenresList/>`, () => {
+  it(`GenresList correctly click catalog__genres-link`, () => {
+    const mockProps = {
+      currentGenreId: 1,
+      genres: [{
+        id: 1,
+        title: `Comedies`,
+      }],
+      onGenreLinkClick: jest.fn(),
+    };
+    const tree = shallow(<GenresList
+      currentGenreId={mockProps.currentGenreId}
+      genres={mockProps.genres}
+      onGenreLinkClick={mockProps.onGenreLinkClick}/>);
+    const genresLink = tree.find(`.catalog__genres-link`).at(0);
+    genresLink.simulate(`click`, {preventDefault() {}});
+    expect(mockProps.onGenreLinkClick).toHaveBeenCalledTimes(1);
+  });
+});
--- src/components/mocks/films.js
+++ src/components/mocks/films.js
@@ -1,25 +1,30 @@
 export const filmsList = [{
   id: 1,
+  genreId: 1,
   title: `Fantastic Beasts`,
   image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
   video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   id: 2,
+  genreId: 2,
   title: `Bohemian Rhapsody`,
   image: `img/bohemian-rhapsody.jpg`,
   video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   id: 3,
+  genreId: 3,
   title: `Macbeth`,
   image: `img/macbeth.jpg`,
   video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   id: 4,
+  genreId: 4,
   title: `Aviator`,
   image: `img/aviator.jpg`,
   video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
 }, {
   id: 5,
+  genreId: 5,
   title: `We need to talk about Kevin`,
   image: `img/we-need-to-talk-about-kevin.jpg`,
   video: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
